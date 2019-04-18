f01102217dd8ef4843e6cf7b3e9bf31f547436f1
Felipe Gutierrez-Barragan <fgutierrez3@wisc.edu>
2019-04-18 17:29:11 -0500

    renamed file

--- VisualizeCodingScheme.py
+++ VisualizeCodingScheme.py
@@ -8,7 +8,7 @@ K = 4
 (ModFs,DemodFs) = CodingFunctions.GetHamK3(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK4(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK5(N = N)
-(ModFs,DemodFs) = CodingFunctions.GetMultiFreqCosK5(N = N)
+# (ModFs,DemodFs) = CodingFunctions.GetMultiFreqCosK5(N = N)
 
 
 UtilsPlot.PlotCodingScheme(ModFs,DemodFs)
