d82b77eac367cc01020912016f6bf271d4e4e1cd
Felipe Gutierrez Barragan <fgutierrez3@wisc.edu>
2019-07-12 12:05:06 -0700

    plot additional functions

--- VisualizeCodingScheme.py
+++ VisualizeCodingScheme.py
@@ -1,17 +1,25 @@
 
 import CodingFunctions
-import UtilsPlot
+from UtilsPlot import *
+from Utils import *
 
 N = 1000
-K = 4
+K = 3
 # (ModFs,DemodFs) = CodingFunctions.GetCosCos(N = N, K = K)
-(ModFs,DemodFs) = CodingFunctions.GetHamK3(N = N)
+(ModFs,DemodFs) = CodingFunctions.GetSqSq(N = N, K = K)
+# (ModFs,DemodFs) = CodingFunctions.GetHamK3(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK4(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetHamK5(N = N)
 # (ModFs,DemodFs) = CodingFunctions.GetMultiFreqCosK5(N = N)
 
+plt.clf()
 
-UtilsPlot.PlotCodingScheme(ModFs,DemodFs)
+CorrFs = GetCorrelationFunctions(ModFs,DemodFs)
+NormCorrFs = NormalizeBrightnessVals(CorrFs)
+
+# PlotCodingScheme(ModFs,DemodFs)
+
+plt.plot(NormCorrFs)
 
 
 
